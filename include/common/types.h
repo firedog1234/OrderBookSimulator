7eaab89a7bff4ade283239e88531b235b81befee
Avi Patel <avipatel@Avis-MacBook-Pro-2.local>
2025-10-31 20:03:06 -0400

    some initial stuff in types and RBtree might have to simplify it

--- include/common/types.h
+++ include/common/types.h
@@ -1,8 +1,24 @@
-//
-// Created by Avi Patel on 10/29/25.
-//
+#pragma once
+#include <cstdint>
 
-#ifndef TYPES_H
-#define TYPES_H
+enum struct Side : uint8_t {BID, ASK};
 
-#endif //TYPES_H
+enum struct OrderType : uint8_t {ADD, CANCEL, MODIFY};
+
+struct alignas(32) Order {
+    int64_t timeStamp;
+    uint32_t id;
+    double price;
+    int32_t quantity;
+    Side side;
+    OrderType type;
+};
+
+/*
+Note that here that the enum values BID, ASK, ADD, ... etc etc will not be initialized to 1,2,3... im p sure
+that you have to reinterperet cast it to an int if you want to use it as an int of you want
+I also used uint8_t b/c other wise the enum would be 4 bytes not 1 and thats j a waste of memory
+and not cache friendly i think for an orderbook.
+alignas(32) just ensures that the padding will be 32 which i think the compiler does anyways. j good practice
+*/
+
--- include/orderbook/RBTree.h
+++ include/orderbook/RBTree.h
@@ -2,15 +2,38 @@
 // Created by Avi Patel on 10/29/25.
 //
 
-#ifndef RBTREE_H
-#define RBTREE_H
+#pragma once
+#include <memory>
+
+#include "../common/types.h"
 
 
+struct RBNode {
+    Order order;
+    RBNode *parent;
+    RBNode *left;
+    RBNode *right;
 
+    explicit RBNode(const Order &order) : order(order), parent(nullptr), left(nullptr), right(nullptr) {}
+};
 class RBTree {
+private:
+    RBNode *root = nullptr;
+    std::vector<std::unique_ptr<RBNode>> node_pool;
+    void rotate_left(RBNode* node);
+    void rotate_right(RBNode* node);
+    constexpr uint32_t SIZE = 100'000;
 
+public:
+    RBTree();
+    void insert(const Order& order);
+    void remove(int order_id);
+    [[nodiscard]] Order get_best_bid() const;
+    [[nodiscard]] Order get_best_ask() const;
 };
 
-
-
-#endif //RBTREE_H
+/* i saw something about preallocating nodes to make it run faster so i think we should do that
+since it is an orderbook
+https://www.youtube.com/watch?v=UOB7-B2MfwA
+The above link is for smart pointer like unique pointer all it does it basically it auto deletes you pointers.
+*/

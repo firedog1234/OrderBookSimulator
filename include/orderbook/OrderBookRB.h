d92dcb6f8c535591bff5e431b42d3da9ab358bc7
Avi Patel <avipatel@Avis-MacBook-Pro-2.local>
2025-11-04 16:20:15 -0500

    sum

---
+++ include/orderbook/OrderBookRB.h
@@ -0,0 +1,16 @@
+//
+// Created by Avi Patel on 11/4/25.
+//
+
+#ifndef ORDERBOOKRB_H
+#define ORDERBOOKRB_H
+
+
+
+class OrderBookRB {
+
+};
+
+
+
+#endif //ORDERBOOKRB_H
---
+++ src/orderbook/OrderBookRB.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Avi Patel on 11/4/25.
+//
+
+#include "../../include/orderbook/OrderBookRB.h"

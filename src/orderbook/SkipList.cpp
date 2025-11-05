74e88d3dce5d7fe0dae36f3bf56b1f54e2aadde6
saishyam <saishyam.js@gmail.com>
2025-11-04 19:32:54 -0500

    RBTree made SIZE static to work with constexpr

--- src/orderbook/SkipList.cpp
+++ src/orderbook/SkipList.cpp
@@ -7,69 +7,43 @@
 skipList:: skipList()
 {
     head = new Node(0, maxNumberOfLevel);    // Initializing the skip list with the max number of levels
-
     Level = 0;                               // At start the level is 0
-
 }
-
-
 
 void skipList::insert(int data)
 {
-    int newLevel = 0;
-
-
-    // Deciding the level of inserting node on the basis of coin toss
-
+    int newLevel = 0; // Deciding the level of inserting node on the basis of coin toss
     while (newLevel < maxNumberOfLevel && (std::rand() % 2) == 1) // here rand()%2 is doing the coin toss
     {
         newLevel++;
-    }
-    // Resizing the size of the levels to make place for the inserting value
-
+    } // Resizing the size of the levels to make place for the inserting value
     if (Level < newLevel)
     {
         head->next.resize(newLevel + 1, nullptr);
-
         Level = newLevel;
     }
-
-
     Node* current = head; // pointer to the head to traverse through the skip list
-
-
-    std::vector<Node*> Update(Level + 1, nullptr); // To store the update node at eah level
+    std::vector<Node*> Update(Level + 1, nullptr); // To store the update node at each level
 
     // Loop over the levels upto which we want the value to be inserted
-
     for (int i = Level; i >= 0; i--)
-    {
-        // Finding the place for the inserting value
-
+    {// Finding the place for the inserting value
         while (current->next[i] and current->next[i]->data < data)
         {
             current = current->next[i];
         }
         // Updating the level accordingly
-
         Update[i] = current;
-
     }
-
     current = current->next[0];   // Moves the current to the next node at level 0
-
     if (current == nullptr || current->data != data) // if the current is null, then it does not exit. we need to insert the value
     {
         Node* newNode = new Node(data, Level);
-
         for (int i = 0; i <= newLevel; i++)
         {
             newNode->next[i] = Update[i]->next[i];
-
             Update[i]->next[i] = newNode;  // To insert the value at each level
-
         }
-
         std::cout << "Element " << data << " inserted successfully.\n";
     }
     else
@@ -78,28 +52,19 @@ void skipList::insert(int data)
     }
 }
 
-
 void skipList::remove(int data)
 {
-    // Function to remove value
     Node* current = head; // start by setting a current pointer to the head node
-
     std::vector<Node*> Update(Level + 1, nullptr); // Create an update vector to store the updated node at each level, Remember only those nodes will be updated where the value to be deleted is present.
-
-
-
     for (int i = Level; i >= 0; i--)
     {
         while (current->next[i] and current->next[i]->data < data)
         {
             current = current->next[i];
         }
-
         Update[i] = current;         // Update array is keeping the track, where the changes should be made, after deleting the node.
     }
-
     current = current->next[0];     // Set the current pointer to the next node at level 0.
-
     if (current != nullptr && current->data == data) // If the value is present, then delete the value
     {
         for (int i = 0; i <= Level; i++)      // Deleting the value from each level
@@ -114,14 +79,11 @@ void skipList::remove(int data)
                 Update[i]->next[i] = current->next[i];
             }
         }
-
         delete current; // deleting the node
-
         while (Level > 0 && head->next[Level] == nullptr)  // decrement the level variable incase there is not any value at that level
         {
             Level--;
         }
-
         std::cout << "Element " << data << " deleted successfully." << std::endl;
     }
     else // Incase the value does not exist
@@ -130,23 +92,17 @@ void skipList::remove(int data)
     }
 }
 
-
 bool skipList::search(int data) const
 {
     Node* current = head;           // start by setting a current pointer to the head node to traverse through the skip list
-
-
     for (int i = Level; i >= 0; i--) // Begin traversing from the top level and iteratively approaching the bottom of the skip list
     {
         while (current->next[i] and current->next[i]->data < data) // keep on moving forward if the value of the next node is less than the searching node otherwise move downward (handled by outer for loop)
         {
             current = current->next[i]; // moving forward
-
         }
     }
-
     current = current->next[0]; // Move to the next of the node at level 0
-
     if (current != nullptr && current->data == data) // if value is found
     {
         std::cout << "Element " << data << " found.\n";
@@ -159,18 +115,13 @@ bool skipList::search(int data) const
     }
 }
 
-
 void skipList::display() const
 {
-
     std::cout << "skip List:" << std::endl;
-
     for (int i = Level; i >= 0; i--) //
     {
         Node* current = head->next[i]; // Initializes the pointer to the first node of that level
-
         std::cout << "Level " << i << ": ";
-
         while (current != nullptr)       // Start displaying all the values present at that level
         {
             std::cout << current->data << " ";
